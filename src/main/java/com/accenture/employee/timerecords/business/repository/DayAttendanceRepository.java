3d25f934bf07b0afb9c5353d7cde0f42203f5148
abhijit.panchal <abhijit.panchal@accenture.com>
2017-04-26 15:14:53 +0530

    changed implementation for dayattendance

--- src/main/java/com/accenture/employee/timerecords/business/repository/DayAttendanceRepository.java
+++ src/main/java/com/accenture/employee/timerecords/business/repository/DayAttendanceRepository.java
@@ -3,13 +3,12 @@ package com.accenture.employee.timerecords.business.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
-import com.accenture.employee.timerecords.business.vo.TimeRecord;
+import com.accenture.employee.timerecords.business.vo.DayAttendance;
 
-public interface DayAttendanceRepository extends MongoRepository<TimeRecord, Integer>{
+public interface DayAttendanceRepository extends MongoRepository<DayAttendance, Integer>{
 		
 	 
-	public List<TimeRecord> findByEmployeeId(Integer empId);
+	public List<DayAttendance> findByEmployeeId(Integer empId);
 
 }
