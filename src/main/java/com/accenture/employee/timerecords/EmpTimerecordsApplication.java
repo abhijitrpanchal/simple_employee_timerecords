0a1e7ac14f2fee068a9a188c9623adeb818cab67
abhijit.panchal <abhijit.panchal@accenture.com>
2017-05-02 14:52:38 +0530

    Checking Turbine implementation

--- src/main/java/com/accenture/employee/timerecords/EmpTimerecordsApplication.java
+++ src/main/java/com/accenture/employee/timerecords/EmpTimerecordsApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 //import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //@SpringBootApplication
 @SpringBootApplication(scanBasePackages = { "com.accenture.employee" })
