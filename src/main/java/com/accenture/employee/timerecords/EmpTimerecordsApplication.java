c4ae796fbc125b7c94c1ee465d30e264885d935b
abhijit.panchal <abhijit.panchal@accenture.com>
2017-05-02 14:44:52 +0530

    Checking Turbine implementation

--- src/main/java/com/accenture/employee/timerecords/EmpTimerecordsApplication.java
+++ src/main/java/com/accenture/employee/timerecords/EmpTimerecordsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+//import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
