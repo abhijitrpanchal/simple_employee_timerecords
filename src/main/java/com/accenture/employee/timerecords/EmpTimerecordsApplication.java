d2f774043d094bcc042db14f5e13ae184bbcba4a
abhijit.panchal <abhijit.panchal@accenture.com>
2017-05-03 10:54:47 +0530

    added @EnableHystrix annotation

--- src/main/java/com/accenture/employee/timerecords/EmpTimerecordsApplication.java
+++ src/main/java/com/accenture/employee/timerecords/EmpTimerecordsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 //import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 //@EnableDiscoveryClient
 @EnableCircuitBreaker
 //@EnableTurbine
+@EnableHystrix
 @EnableHystrixDashboard 
 @Component
 @Service
